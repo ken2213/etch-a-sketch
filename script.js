20c1937054ef2521955fd5d5498701f9e49ddfa3
Kenneth Ornieta <me@kennethornieta.com>
2022-08-07 20:34:26 +0800

    working grid size

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-const container = document.getElementById("container");
-const customizeButton = document.getElementById("customize-button");
+//Selecting "etch-tablet" id from HTML
+const etchTablet = document.getElementById('etch-tablet')
+//Selecting "grid-container" id from HTML
+const gridContainer = document.getElementById('grid-container');
+//Selecting all button element from HTML
+const blackButton = document.getElementById('button-black');
+const rainbowButton = document.getElementById('button-rainbow');
+const shadeButton = document.getElementById('button-shade');
+
+const clearButton = document.getElementById('button-clear');
+const resizeButton = document.getElementById('button-resize');
+
+let pixel = '';
+let gridSize = 16;
+
+const drawGrid = (screenSize) => {
+  for(i = 0; i < screenSize ** 2; i++) {
+    pixel = document.createElement('div'); // creates a div element
+    pixel.classList.add('pixel'); //adds a class on div element
+    pixel.style.backgroundColor = 'hsla(0, 0, 0, 50%)'; // add background color to element
+    pixel.style.border = "1px solid #ddd" // add border size, type, and color
+    gridContainer.appendChild(pixel); // puts the div as a child of gridContainer
+  }
+  //repeating depending on the value of screensize and having a automatic width and height
+  gridContainer.style.gridTemplateColumns = `repeat(${screenSize}, auto)`;
+  gridContainer.style.gridTemplateRows = `repeat(${screenSize}, auto)`
+
+}
+
+drawGrid(gridSize);
+
+
+
+
+
+
 
 
 /*
@@ -19,8 +53,8 @@ customizeButton.addEventListener('click', () => {
 */
 
 
-
-function makeRows(rows, columns) {
+/*
+function makeGrid(rows, columns) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-columns', columns);
   for (c = 0; c < (rows * columns); c++) {
@@ -33,7 +67,7 @@ function makeRows(rows, columns) {
 
 
 makeRows(10, 10);
-
+*/
 
 
 
