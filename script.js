32c25797c64e98433f1356e4c4b51fa336ccd065
Kenneth Ornieta <me@kennethornieta.com>
2022-08-08 18:09:01 +0800

    working code

--- script.js
+++ script.js
@@ -3,90 +3,124 @@ const etchTablet = document.getElementById('etch-tablet')
 //Selecting "grid-container" id from HTML
 const gridContainer = document.getElementById('grid-container');
 //Selecting all button element from HTML
-const blackButton = document.getElementById('button-black');
-const rainbowButton = document.getElementById('button-rainbow');
-const shadeButton = document.getElementById('button-shade');
+const colorButtons = document.querySelectorAll('.color-choice');
 
 const clearButton = document.getElementById('button-clear');
 const resizeButton = document.getElementById('button-resize');
 
-let pixel = '';
-let gridSize = 16;
+var color = 'black';
 
-const drawGrid = (screenSize) => {
-  for(i = 0; i < screenSize ** 2; i++) {
-    pixel = document.createElement('div'); // creates a div element
-    pixel.classList.add('pixel'); //adds a class on div element
-    pixel.style.backgroundColor = 'hsla(0, 0, 0, 50%)'; // add background color to element
-    pixel.style.border = "1px solid #ddd" // add border size, type, and color
-    gridContainer.appendChild(pixel); // puts the div as a child of gridContainer
+
+
+function drawGrid(gridNumber) {
+  let gridArea = gridNumber * gridNumber;
+  for (let i = 1; i <= gridArea; i++) {
+    let gridPixel = document.createElement('div');
+    gridPixel.classList.add('grid-pixel');
+
+
+    gridContainer.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
+    gridContainer.insertAdjacentElement('beforeend', gridPixel);
   }
-  //repeating depending on the value of screensize and having a automatic width and height
-  gridContainer.style.gridTemplateColumns = `repeat(${screenSize}, auto)`;
-  gridContainer.style.gridTemplateRows = `repeat(${screenSize}, auto)`
 
+  var gridPixels = gridContainer.querySelectorAll('div');
+  gridPixels.forEach(gridPixel => gridPixel.addEventListener('mouseover', colorGrid));
 }
 
-drawGrid(gridSize);
+
+function colorGrid() {
+  switch (color) {
+
+    case 'rainbow':
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+      this.classList.remove('gray');
+      break;
+    
+    case 'gray':
+      if (this.style.backgroundColor.match(/rgba/)) {
+        let currentOpacity = Number(this.style.backgroundColor.slice(-4, -1));
+        if (currentOpacity <= 0.9) {
+            this.style.backgroundColor = `rgba(128, 128, 128, ${currentOpacity + 0.1})`;
+            this.classList.add('gray');
+        }
+      } else if (this.classList == 'gray' && this.style.backgroundColor == 'rgb(128, 128, 128)') {
+         return;
+      } else {
+         this.style.backgroundColor = 'rgba(128, 128, 128, 0.1)';  
+      }
+      break;
+
+    case 'eraser':
+      this.style.backgroundColor = 'hsla(0, 0%, 0%, 0%)';
+      this.classList.remove('gray');
+      break;
+
+    case 'black':
+      this.style.backgroundColor = '#000000';
+      this.classList.remove('gray');
+      break;
+
+    default:
+      this.style.backgroundColor = color;
+      this.classList.remove('gray');
+      break;
+  }
+}
+
+
+// Clear Button
+function eraseAllColor() {
+  var gridPixels = gridContainer.querySelectorAll('div');
+  gridPixels.forEach(gridPixel => gridPixel.style.backgroundColor = 'hsla(0, 0%, 0%, 0%)');
+}
 
 
 
+// Updates color variable when a color button is clicked
+function changeColor(event) {
+  switch (event.target.dataset.color) { 
+      case 'rainbow':
+          color = 'rainbow';
+          break;  
+      case 'gray':
+          color = 'gray';
+          break;
+      case 'eraser':
+          color = 'eraser';
+          break;
+      default:
+          color = 'black';
+          break;
+  } 
+}
 
 
 
-
-
-/*
-customizeButton.addEventListener('click', () => {
-  let gridSize = prompt("Enter how wide you wanna color");
+function resize () {
+  let gridNumber = prompt("Enter how wide you wanna color");
   // error prompt conditionals
-  while (gridSize > 100 || gridSize < 2 || isNaN(gridSize)) {
-    if (isNaN(gridSize)) {
-      gridSize = prompt("Enter a number only, ranges from 2-100");
-    } else if (gridSize === null) {
+  while (gridNumber > 100 || gridNumber < 2 || isNaN(gridNumber)) {
+    if (isNaN(gridNumber)) {
+      gridNumber = prompt("Enter a number only, ranges from 2-100");
+    } else if (gridNumber === null) {
       break;
     } else {
-      gridSize = prompt("Please pick a number between 2 and 100!")
+      gridNumber = prompt("Please pick a number between 2 and 100!", 16)
     }
   }
-})
-*/
-
-
-/*
-function makeGrid(rows, columns) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-columns', columns);
-  for (c = 0; c < (rows * columns); c++) {
-    let cell = document.createElement("div");
-    cell.addEventListener('mouseover',
-     (e) => e.target.classList.add('my-color-class'))
-    container.appendChild(cell).className = "grid-item";
-  };
-};
-
-
-makeRows(10, 10);
-*/
+  eraseAllColor();
+  drawGrid(gridNumber);
+}
 
 
 
 
+clearButton.addEventListener('click', eraseAllColor);
+colorButtons.forEach(colorButton => colorButton.addEventListener('click', changeColor));
+resizeButton.addEventListener('click', resize);
 
 
+//default grid
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+drawGrid(10);
